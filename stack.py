e6ee7342d8d939b2ba79c3e2e29cd7aeac6e4285
tylerlongo <tylerjlongo9@gmail.com>
2023-09-06 21:52:26 -0400

    updated variable names

--- stack.py
+++ stack.py
@@ -1,21 +1,21 @@
 class Stack:
     def __init__(self):
-        self.__stack = list()
+        self.items = list()
 
     def push(self, val: int) -> None:
-        self.__stack.append(val)
+        self.items.append(val)
     
     def pop(self) -> None:
-        self.__stack.pop()
+        self.items.pop()
 
     def top(self) -> int:
-        return self.__stack[-1]
+        return self.items[-1]
     
-    def items(self) -> list:
-        return self.__stack
+    def getItems(self) -> list:
+        return self.items
     
 stack = Stack()
 for num in range(5):
     stack.push(num)
 stack.pop()
-print(stack.items())
+print(stack.getItems())
